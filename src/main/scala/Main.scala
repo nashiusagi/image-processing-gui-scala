c4c218ea366ae2d5dfad2bcc9378b2da5f993061
nashiusagi <ritotech0226@gmail.com>
2023-09-17 15:13:47 +0900

    [add] image and pixel class

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,9 +3,12 @@ import scalafx.geometry.Insets
 import scalafx.scene.Scene
 import scalafx.scene.control.Label
 import scalafx.scene.layout.BorderPane
+import img.Image
 
 object HelloSBT extends JFXApp3 {
   override def start(): Unit = {
+    val image: Image = new Image("./resources/sample.png")
+    println(image.pixels(24 * 50 + 24).getColor())
     stage = new JFXApp3.PrimaryStage {
       scene = new Scene {
         root = new BorderPane {
