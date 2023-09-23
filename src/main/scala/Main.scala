d91bd810e7d62c801c80c27443fd391b2ca0626b
nashiusagi <ritotech0226@gmail.com>
2023-09-23 11:33:47 +0900

    [update] util methods to package object

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -7,24 +7,22 @@ import scalafx.scene.layout.Pane
 import scalafx.scene.image.WritableImage
 import scalafx.embed.swing.SwingFXUtils
 import img.filtering.GaussianFilter
-import scala.util.Try
 import scala.util.Success
 import scala.util.Failure
 import java.awt.image.BufferedImage
 import java.awt.image.BufferedImage.TYPE_INT_ARGB
-import java.io.File
-import javax.imageio.ImageIO
 import img.Pixel
 import scalafx.scene.control.Button
 import scalafx.scene.layout.VBox
+import img._
 
 object HelloSBT extends JFXApp3 {
   override def start(): Unit = {
-    val bufImage: BufferedImage = load("./resources/sample.png") match {
+    val bufImage: BufferedImage = img.load("./resources/sample.png") match {
       case Success(src) => src
       case Failure(_)   => new BufferedImage(128, 128, TYPE_INT_ARGB)
     }
-    val origPixels: List[Pixel] = bufferedImage2Pixels(bufImage)
+    val origPixels: List[Pixel] = img.bufferedImageToPixels(bufImage)
     val image: Image =
       new Image(origPixels, bufImage.getWidth, bufImage.getHeight)
 
@@ -56,25 +54,6 @@ object HelloSBT extends JFXApp3 {
 
     stage = new JFXApp3.PrimaryStage {
       scene = new Scene(root)
-    }
-  }
-
-  def load(filename: String): Try[BufferedImage] = {
-    val file: File = new File(filename)
-
-    Try(ImageIO.read(file))
-  }
-
-  def bufferedImage2Pixels(src: BufferedImage): List[Pixel] = {
-    val width: Int = src.getWidth
-    val height: Int = src.getHeight
-
-    for (iter <- (0 until width * height).toList) yield {
-      val x = iter % width
-      val y = iter / height
-      val color = src.getRGB(x, y)
-
-      new Pixel(x, y, color)
     }
   }
 }
