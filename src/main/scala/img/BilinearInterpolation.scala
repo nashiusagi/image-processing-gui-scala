9beeb118d9b3c0418fb6c44bbbd4192a0a0e1aac
nashiusagi <ritotech0226@gmail.com>
2023-09-24 17:09:33 +0900

    [fix] fix edge bug

--- src/main/scala/img/BilinearInterpolation.scala
+++ src/main/scala/img/BilinearInterpolation.scala
@@ -16,26 +16,35 @@ class BilinearInterpolation(rx: Double, ry: Double) {
     val interpolatedPixels: List[Pixel] =
       for (y <- (0 until aH).toList; x <- (0 until aW).toList) yield {
         val yBefore = Math.min(Math.floor(y / ay).toInt, srcHeight - 2)
-        val dy = (y / ay).toInt - yBefore
+        val dy: Double = ((y / ay).toInt - yBefore).toDouble
 
         val xBefore = Math.min(Math.floor(x / ax).toInt, srcWidth - 2)
-        val dx = (x / ax).toInt - xBefore
+        val dx: Double = ((x / ax).toInt - xBefore).toDouble
 
         // compute bi-linear
-        val red = (1 - dx) * (1 - dy) * src.getPixel(xBefore, yBefore).red
-        +dx * (1 - dy) * src.getPixel(xBefore + 1, yBefore).red
-        +(1 - dx) * dy * src.getPixel(xBefore, yBefore + 1).red
-        +dx * dy * src.getPixel(xBefore + 1, yBefore + 1).red
+        val red: Double = (1.0 - dx) * (1.0 - dy) * src
+          .getPixel(xBefore, yBefore)
+          .red + dx * (1.0 - dy) * src
+          .getPixel(xBefore + 1, yBefore)
+          .red + (1.0 - dx) * dy * src
+          .getPixel(xBefore, yBefore + 1)
+          .red + dx * dy * src.getPixel(xBefore + 1, yBefore + 1).red
 
-        val green = (1 - dx) * (1 - dy) * src.getPixel(xBefore, yBefore).green
-        +dx * (1 - dy) * src.getPixel(xBefore + 1, yBefore).green
-        +(1 - dx) * dy * src.getPixel(xBefore, yBefore + 1).green
-        +dx * dy * src.getPixel(xBefore + 1, yBefore + 1).green
+        val green: Double = (1 - dx) * (1 - dy) * src
+          .getPixel(xBefore, yBefore)
+          .green + dx * (1 - dy) * src
+          .getPixel(xBefore + 1, yBefore)
+          .green + (1 - dx) * dy * src
+          .getPixel(xBefore, yBefore + 1)
+          .green + dx * dy * src.getPixel(xBefore + 1, yBefore + 1).green
 
-        val blue = (1 - dx) * (1 - dy) * src.getPixel(xBefore, yBefore).blue
-        +dx * (1 - dy) * src.getPixel(xBefore + 1, yBefore).blue
-        +(1 - dx) * dy * src.getPixel(xBefore, yBefore + 1).blue
-        +dx * dy * src.getPixel(xBefore + 1, yBefore + 1).blue
+        val blue: Double = ((1.0 - dx) * (1.0 - dy) * src
+          .getPixel(xBefore, yBefore)
+          .blue) + (dx * (1.0 - dy) * src
+          .getPixel(xBefore + 1, yBefore)
+          .blue) + ((1.0 - dx) * dy * src
+          .getPixel(xBefore, yBefore + 1)
+          .blue) + (dx * dy * src.getPixel(xBefore + 1, yBefore + 1).blue)
 
         val newColor =
           0xff000000 + ((red * 255).toInt << 16) + ((green * 255).toInt << 8) + (blue * 255).toInt
